a1649156bc0464f9f97e1bd6b592f19b91921975
anhelinasmuhliy <smuhliy07@gmail.com>
2023-05-14 11:47:15 +0200

    Add.

---
+++ models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Integer, Column, String, create_engine
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine(
+    "mysql+pymysql://root:12345A@localhost:3306/company"
+)
+Base = declarative_base(bind=engine)
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    first_name = Column(String(50), nullable=False)
+    last_name = Column(String(50), nullable=False)
+    email = Column(String(50), nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"<User {self.first_name} {self.last_name}>"
+
+
+Base.metadata.create_all()
